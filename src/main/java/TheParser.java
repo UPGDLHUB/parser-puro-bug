c9cae7726b4fdd2c7a93543f2b18f3f742c70f8a
GTorreZac <100299138+GTorreZac@users.noreply.github.com>
2025-03-27 18:49:23 -0600

    Update TheParser.java

--- src/main/java/TheParser.java
+++ src/main/java/TheParser.java
@@ -1,5 +1,7 @@
 import java.util.Vector;
-import path.to.Lexer;
+import Lexer;
+import Automata; // Si está en el mismo paquete
+import TheToken; // Si está en el mismo paquete
 
 public class TheParser {
     private Vector<TheToken> tokens;
